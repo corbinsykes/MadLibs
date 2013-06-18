529c7a5eac2545b29340eed5f3be1f67807bf96c
Corbin Sykes <corbin.sykes@gmail.comm>
2013-06-18 12:14:04 -0400

    MadLib first draft

---
+++ madlibs.rb
@@ -0,0 +1,33 @@
+puts "Give me a noun"
+first_noun = gets.chomp
+
+puts "Give me a verb"
+first_verb = gets.chomp
+
+puts "Name a celebrity"
+celebrity = gets.chomp
+
+puts "Name a car"
+car = gets.chomp
+
+puts "What's your favorite city?"
+city = gets.chomp
+
+puts "Name an animal"
+animal = gets.chomp
+
+puts "I need another verb"
+second_verb = gets.chomp
+
+puts "Name a fruit"
+fruit = gets.chomp
+
+puts "Name a drink"
+drink = gets.chomp
+
+puts "Give me a past tense verb"
+third_verb = gets.chomp
+
+puts "A man wanted to #{first_verb} his #{first_noun}. He thought about what #{celebrity} would do in this situation. " +
+"So he hopped in his #{car} and drove to #{city}. There he found #{celebrity} with a #{animal}. The #{animal} was eating a #{fruit}. " +
+"I want to #{first_verb} my #{first_noun}, #{celebrity}. What should I do? Have a #{drink}, #{celebrity} said. Then s/he #{third_verb}."
